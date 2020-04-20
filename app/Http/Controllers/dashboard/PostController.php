c740e8d3ba0361f3ccef074bce243da8d49d913e
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-04-20 17:50:56 +0200

    mover las validaciones del formulario a otra carpeta Request

--- app/Http/Controllers/dashboard/PostController.php
+++ app/Http/Controllers/dashboard/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\dashboard;
 use App\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostPost;
 
 class PostController extends Controller
 {
@@ -34,18 +35,19 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StorePostPost $request)
     {
-        $request->validate([
-            'title' => 'required|min:5|max:500',
-            //'url_clean'=>'required|min:5|max:500',
-            'content' => 'required|min:5'
-        ]);
+        //dd($request->validated());
+        //$request->validate([
+        //   'title' => 'required|min:5|max:500',
+        //'url_clean'=>'required|min:5|max:500',
+        //    'content' => 'required|min:5'
+        //]);
 
         echo "Hola Store: " . $request->title;
-        
 
-        //Post::create();
+
+        Post::create($request->validated());
         //echo "Hola Store: " .$request("title");
     }
 
