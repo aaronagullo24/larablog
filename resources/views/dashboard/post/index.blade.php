fe0321622b6c45bb8552e63abb5b80d20cfef7f8
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-04-21 17:45:26 +0200

    borrar post con dialogo de confirmacion

--- resources/views/dashboard/post/index.blade.php
+++ resources/views/dashboard/post/index.blade.php
@@ -52,11 +52,10 @@
             <td>
                 <a href="{{ route('post.show',$post->id)}}" class="btn btn-primary">Ver</a>
                 <a href="{{ route('post.edit',$post->id)}}" class="btn btn-primary">Actualizar</a>
-                <form method="post" action="{{ route('post.destroy',$post->id)}}">
-                    @method('DELETE')
-                    @csrf
-                    <button class="btn btn-danger" type="submit">Delete</button>
-                </form>
+
+                <button class="btn btn-danger" type="submit" data-toggle="modal" data-target="#deleteModal"
+                    data-id="{{ $post->id }}">Delete</button>
+
             </td>
         </tr>
         @endforeach
@@ -65,4 +64,47 @@
 </table>
 
 {{$posts->links()}}
+
+
+
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="ModalLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Seguro que desea borrar el registro?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                <form id="formDelete" method="post" action="{{ route('post.destroy',0)}}" data-action="{{route('post.destroy',0)}}">
+                    @method('DELETE')
+                    @csrf
+                    <button type="submit" class="btn btn-danger">Borrar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    $('#deleteModal ').on('show.bs.modal', function (event) {
+
+        var button = $(event.relatedTarget)
+        var id = button.data('id')
+        action = $('#formDelete').attr('data-action').slice(0, -1)
+        action+=id
+        $('#formDelete').attr('action',action)
+        console.log(action)
+        var modal = $(this)
+        modal.find('.modal-title').text('vas a borrar el post: ' + id)
+
+    })
+</script>
+
 @endsection
