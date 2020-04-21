3cb53304ddf7e0a2422ce1d24579aab0a164dc32
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-04-21 16:29:49 +0200

    valores por defecto en actualizacion

--- resources/views/dashboard/post/_form.blade.php
+++ resources/views/dashboard/post/_form.blade.php
@@ -3,7 +3,7 @@
     
         <div class="form-group">
             <label for="title">Titulo</label>
-            <input class="form-control" type="text" id="title" name="title" value={{old('title')}}>
+            <input class="form-control" type="text" id="title" name="title" value={{old('title',$post->title)}}>
             @error('title')
                 <small class="text-danger">{{$message}}</small>
             @enderror
@@ -12,12 +12,12 @@
     
         <div class="form-group">
             <label for="url_clean">url limpia</label>
-            <input class="form-control" type="text" id="url_clean" name="url_clean" value={{old('url_clean')}}>
+            <input class="form-control" type="text" id="url_clean" name="url_clean" value={{old('url_clean',$post->url_clean)}}>
         </div>
        
         <div class="form-group">
             <label for="content">Contenido</label>
-            <textarea class="form-control" id="content" name="content" row="3">{{old('content')}}</textarea>
+            <textarea class="form-control" id="content" name="content" row="3">{{old('content',$post->content)}}</textarea>
         </div>
     
         <input type="submit" value="Enviar" class="btn btn-primary">
