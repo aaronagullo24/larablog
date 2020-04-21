efc9d677c6bd22fefb485cccd45382a9757d5565
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-04-21 18:57:16 +0200

    instalar CKEditor

--- resources/views/dashboard/post/master.blade.php
+++ resources/views/dashboard/post/master.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="{{asset("css/app.css")}}">
-  <script src="{{ asset("js/app.js") }}"></script>
+ 
   <title> Modulo </title>
 </head>
 
@@ -19,7 +19,7 @@
     @include('dashboard.parciales.session-flash-status')
     @yield('content')
   </div>
-  
+  <script src="{{ asset("js/app.js") }}"></script>
 </body>
 
 </html>
