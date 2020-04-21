9e3a3711dde1a162a1b07aac3d409fcc6c8e6bb7
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-04-21 19:12:23 +0200

    condigurar categorias

--- resources/views/dashboard/post/_form.blade.php
+++ resources/views/dashboard/post/_form.blade.php
@@ -1,24 +1,34 @@
+@csrf
 
-        @csrf
-    
-        <div class="form-group">
-            <label for="title">Titulo</label>
-            <input class="form-control" type="text" id="title" name="title" value={{old('title',$post->title)}}>
-            @error('title')
-                <small class="text-danger">{{$message}}</small>
-            @enderror
-        </div>
-    
-    
-        <div class="form-group">
-            <label for="url_clean">url limpia</label>
-            <input class="form-control" type="text" id="url_clean" name="url_clean" value={{old('url_clean',$post->url_clean)}}>
-        </div>
-       
-        <div class="form-group">
-            <label for="content">Contenido</label>
-            <textarea class="form-control" id="content" name="content" row="3">{{old('content',$post->content)}}</textarea>
-        </div>
-    
-        <input type="submit" value="Enviar" class="btn btn-primary">
-  
+<div class="form-group">
+    <label for="title">Titulo</label>
+    <input class="form-control" type="text" id="title" name="title" value={{old('title',$post->title)}}>
+    @error('title')
+    <small class="text-danger">{{$message}}</small>
+    @enderror
+</div>
+
+
+<div class="form-group">
+    <label for="url_clean">url limpia</label>
+    <input class="form-control" type="text" id="url_clean" name="url_clean" value={{old('url_clean',$post->url_clean)}}>
+</div>
+
+
+<div class="form-group">
+    <label for="categorias">Categorias</label>
+
+    <select name="category_id" class="form-control" id="category_id">
+        @foreach($categories as $title=>$id)
+            <option value="{{$id}}">{{$title}}</option>
+        @endforeach
+    </select>
+
+</div>
+
+<div class="form-group">
+    <label for="content">Contenido</label>
+    <textarea class="form-control" id="content" name="content" row="3">{{old('content',$post->content)}}</textarea>
+</div>
+
+<input type="submit" value="Enviar" class="btn btn-primary">
