09ec23743149acc81619b55a053eb9b04bef40d6
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-04-28 18:31:14 +0200

    comentarios en un model definir el modal

--- resources/views/dashboard/post-comment/post.blade.php
+++ resources/views/dashboard/post-comment/post.blade.php
@@ -5,17 +5,17 @@
     <form action="{{ route('post-comment.post',1)}}" id="FilterForm">
         <div class="form-row">
             <div class="col-10">
-            <select id="filterPost" class="form-control">
-                @foreach ($posts as $p)
-                <option value="{{$p->id}}" {{$post->id == $p->id ? 'selected' : ''}}>{{Str::limit($p->title,80)}}
-                </option>
-                @endforeach
-            </select>
+                <select id="filterPost" class="form-control">
+                    @foreach ($posts as $p)
+                    <option value="{{$p->id}}" {{$post->id == $p->id ? 'selected' : ''}}>{{Str::limit($p->title,80)}}
+                    </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="col-2">
+                <button type="submit" class="btn btn-success">Enviar</button>
+            </div>
         </div>
-        <div class="col-2">
-            <button type="submit" class="btn btn-success">Enviar</button>
-        </div>
-    </div>
     </form>
 </div>
 @if (count($postComments) > 0)
@@ -68,7 +68,10 @@
                 {{ $postComment->updated_at->format('d-m-Y') }}
             </td>
             <td>
-                <a href="{{ route('post-comment.show',$postComment->id) }}" class="btn btn-primary">Ver</a>
+
+
+                <button data-toggle="modal" data-target="#showModal" data-id="{{ $postComment->id }}"
+                    class="btn btn-primary">ver</button>
 
                 <button data-toggle="modal" data-target="#deleteModal" data-id="{{ $postComment->id }}"
                     class="btn btn-danger">Eliminar</button>
@@ -110,14 +113,51 @@
     </div>
 </div>
 
+
+<div class="modal fade" id="showModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p class="message"></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+
+            </div>
+        </div>
+    </div>
+</div>
+
 <script>
     window.onload = function () {
+        $('#showModal').on('show.bs.modal', function (event) {
+
+            var button = $(event.relatedTarget)
+            var id = button.data('id')
+            var modal = $(this)
+            $.ajax({
+                method: "GET",
+                url: "{{URL::to(" / ")}}/dashboard/post-comment/j-show/" + id,
+            })
+                .done(function (comment) {
+                    modal.find('.modal-title').text(comment.title)
+                    modal.find('.message').text(comment.message)
+                })
+        });
+
+
+
         $('#deleteModal').on('show.bs.modal', function (event) {
 
-            var button = $(event.relatedTarget) // Button that triggered the modal
-            var id = button.data('id') // Extract info from data-* attributes
-            // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-            // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+            var button = $(event.relatedTarget)
+            var id = button.data('id')
 
             action = $('#formDelete').attr('data-action').slice(0, -1)
             action += id
@@ -138,12 +178,12 @@
 @endif
 
 <script>
-    window.onload=function(){
-        $("#FilterForm").submit(function(){
-            var action=$(this).attr('action');
-            action=action.replace(/[0-9]/g,$("#filterPost").val());
+    window.onload = function () {
+        $("#FilterForm").submit(function () {
+            var action = $(this).attr('action');
+            action = action.replace(/[0-9]/g, $("#filterPost").val());
             console.log(action);
-            $(this).attr('action',action)
+            $(this).attr('action', action)
         });
     }
 </script>
