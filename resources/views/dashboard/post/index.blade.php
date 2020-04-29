c46fbfdd077f11dc10b222a4c225dbdb4cceaa24
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-04-29 17:27:40 +0200

    creando el formulario para el filtro y campo de ordenacion

--- resources/views/dashboard/post/index.blade.php
+++ resources/views/dashboard/post/index.blade.php
@@ -6,6 +6,15 @@
     Crear
 </a>
 
+<form action="{{route('post.index')}}" class="form-inline mb-2">
+    <select name="created_at" class="form-control">
+        <option value="DESC">Descendente</option>
+        <option {{ request('created_at')== "ASC" ? "selected" : ""}} value="ASC">Ascendente</option>
+    </select>
+    <input type="text" value="{{request('search')}}" name="search" placeholder="Buscar..." class="form-control ml-1">
+    <button type="submit" class="btn btn-success ml-2">Enviar</button>
+</form>
+
 <table class="table">
     <thead>
         <tr>
