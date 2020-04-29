94f12e70a054c22cccace4aed393321be3651420
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-04-29 21:30:38 +0200

    redireccionar a un modulo dependiendo de tol del usuario autenticado

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -36,5 +37,21 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectTo()
+    {
+        $rol = Auth::user()->rol->key;
+
+        switch ($rol) {
+            case 'admin':
+                return '/dashboard/post';
+                break;
+
+            default:
+                return '/';
+
+                break;
+        }
     }
 }
