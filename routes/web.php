53c65f30a3c2a0df52a6d08c56b228a2757eb871
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-05-07 12:01:37 +0200

    fluent string

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,11 +17,18 @@ use Illuminate\Support\Facades\Route;
 
 
 
-/*
-Route::get('/test', function () {
-    return "hola mundo";
-});
 
+Route::get('/test', function () {
+    
+    $string = "      hola mundo Aaron";
+    //$string=strtolower(trim($string));    
+    //$string = Str::of($string)->afterLast("o")->ascii();
+
+    $string = Str::of($string)->trim()->lower()->replace(" ","-")->ascii();
+    
+    return $string;
+});
+/*
 Route::get('/hola/{nombre?}', function ($nombre = "juan") {
     return "hola $nombre conocenos: <a href='".route("nosotros")."'>nosotros</a>" ;
 });
@@ -42,40 +50,40 @@ Route::get('home/{nombre?}/{apellido?}', function($nombre="Defecto",$apellido="M
 
 Route::resource('dashboard/post', 'dashboard\PostController');
 Route::resource('dashboard/category', 'dashboard\CategoryController');
-Route::post('dashboard/post/{post}/image','dashboard\PostController@image')->name('post.image');
+Route::post('dashboard/post/{post}/image', 'dashboard\PostController@image')->name('post.image');
 Route::resource('dashboard/user', 'dashboard\UserController');
 
-Route::get('dashboard/post/image-download/{image}','dashboard\PostController@imageDownload')->name('post.image-download');
+Route::get('dashboard/post/image-download/{image}', 'dashboard\PostController@imageDownload')->name('post.image-download');
 
-Route::delete('dashboard/post/image-delete/{image}','dashboard\PostController@imageDelete')->name('post.image-delete');
+Route::delete('dashboard/post/image-delete/{image}', 'dashboard\PostController@imageDelete')->name('post.image-delete');
 
 Route::resource('dashboard/contact', 'dashboard\ContactController')->only([
-    'index','show','destroy'
+    'index', 'show', 'destroy'
 ]);
 
 Route::resource('dashboard/post-comment', 'dashboard\PostCommentController')->only([
-    'index','show','destroy'
+    'index', 'show', 'destroy'
 ]);
 
-Route::get('dashboard/post-comment/{post}/post','dashboard\PostCommentController@post')->name('post-comment.post');
+Route::get('dashboard/post-comment/{post}/post', 'dashboard\PostCommentController@post')->name('post-comment.post');
 
-Route::get('dashboard/post-comment/j-show/{postComment}','dashboard\PostCommentController@jshow');
+Route::get('dashboard/post-comment/j-show/{postComment}', 'dashboard\PostCommentController@jshow');
 
-Route::post('dashboard/post-comment/proccess/{postComment}','dashboard\PostCommentController@proccess');
+Route::post('dashboard/post-comment/proccess/{postComment}', 'dashboard\PostCommentController@proccess');
 
-Route::post('dashboard/post/content_image','dashboard\PostController@contentImage');
+Route::post('dashboard/post/content_image', 'dashboard\PostController@contentImage');
 
-Route::get('/','web\WebController@index' )->name('index');
+Route::get('/', 'web\WebController@index')->name('index');
 
-Route::get('/categories','web\WebController@index' )->name('index');
+Route::get('/categories', 'web\WebController@index')->name('index');
 
-Route::get('/detail/{id}','web\WebController@detail');
+Route::get('/detail/{id}', 'web\WebController@detail');
 
-Route::get('/post-category/{id}','web\WebController@post_category');
+Route::get('/post-category/{id}', 'web\WebController@post_category');
 
-Route::get('dashboard/excel/post-export','dashboard\PostController@export')->name('post.export');
+Route::get('dashboard/excel/post-export', 'dashboard\PostController@export')->name('post.export');
 
-Route::get('/contact','web\WebController@contact');
+Route::get('/contact', 'web\WebController@contact');
 
 Auth::routes();
 
@@ -84,12 +92,12 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/image', 'PaquetesController@image')->name('image');
 Route::get('/chart', 'PaquetesController@charts')->name('chart');
 Route::get('/qr_generate', 'PaquetesController@qr_generate')->name('qr_generate');
-Route::get('dashboard/excel/post-import','dashboard\PostController@import');
-Route::get('/translate','PaquetesController@translate')->name('translate');
-Route::get('/strip_create_customer','PaquetesController@strip_create_customer')->name('strip_create_customer');
-Route::get('/stripe_payment_method_register','PaquetesController@stripe_payment_method_register')->name('stripe_payment_method_register');
-Route::get('/stripe_payment_method_create','PaquetesController@stripe_payment_method_create')->name('stripe_payment_method_create');
-Route::get('/stripe_payment_method','PaquetesController@stripe_payment_method')->name('stripe_payment_method');
-Route::get('/stripe_create_only_pay_form','PaquetesController@stripe_create_only_pay_form')->name('stripe_create_only_pay_form');
-Route::get('/stripe_create_only_pay','PaquetesController@stripe_create_only_pay')->name('stripe_create_only_pay');
-Route::get('/stripe_create_suscription','PaquetesController@stripe_create_suscription')->name('stripe_create_suscription');
+Route::get('dashboard/excel/post-import', 'dashboard\PostController@import');
+Route::get('/translate', 'PaquetesController@translate')->name('translate');
+Route::get('/strip_create_customer', 'PaquetesController@strip_create_customer')->name('strip_create_customer');
+Route::get('/stripe_payment_method_register', 'PaquetesController@stripe_payment_method_register')->name('stripe_payment_method_register');
+Route::get('/stripe_payment_method_create', 'PaquetesController@stripe_payment_method_create')->name('stripe_payment_method_create');
+Route::get('/stripe_payment_method', 'PaquetesController@stripe_payment_method')->name('stripe_payment_method');
+Route::get('/stripe_create_only_pay_form', 'PaquetesController@stripe_create_only_pay_form')->name('stripe_create_only_pay_form');
+Route::get('/stripe_create_only_pay', 'PaquetesController@stripe_create_only_pay')->name('stripe_create_only_pay');
+Route::get('/stripe_create_suscription', 'PaquetesController@stripe_create_suscription')->name('stripe_create_suscription');
